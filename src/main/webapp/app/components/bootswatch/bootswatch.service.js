f4e1fd0cd46d12fdf1d98725c7978d757a89faa4
mohantangirala-iit <mtangirala@hawk.iit.edu>
2024-07-30 19:52:08 +0000

    Added theme switcher

---
+++ src/main/webapp/app/components/bootswatch/bootswatch.service.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('eventsOnline').factory('BootSwatchService', function ($http) {
+  return {
+    get: function () {
+      return $http.get('http://bootswatch.com/api/3.json').then(function (response) {
+        return response.data.themes;
+      });
+    },
+  };
+});
